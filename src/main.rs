6b2daa4aeec7b65127584da01feb1da2466d594f
Elgius <114131653+Elgius@users.noreply.github.com>
2023-11-06 10:35:30 +0500

    added new functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io;
-use std::fs;
 use colored::Colorize;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::Write;
 
 fn completed_logs() {
     println!("this is a incomplete function");
@@ -11,49 +13,82 @@ fn delete_items() {
 }
 
 fn add_items() {
-    println!("this is a incomplete function");
+    println!("welcome to the add items menu!");
+    let welcome_message = "please add what you want to do today!:   ".blue();
+    println!("{}", welcome_message);
+    let mut items: String = String::new();
+    io::stdin().read_line(&mut items).expect("value missing");
+    // file appending function that creates a new file if file is missing. otherwise it add the values directly into txt file
+    // works more efficiently than i thought, pretty cool
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("todo.txt")
+        .expect("file missing");
+
+    file.write_all(items.as_bytes())
+        .expect("error occured at the appending stage!");
+    println!("your new tasks have been appended successfully");
+
+    println!("do you want to see the new changes? [y,n]");
+
+    let mut choice = String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("error in getting choices");
+
+    // allows the user to go back to the main menu and check the progress
+    // BUG: 'y' and 'n' dont work, so it always go to default value
+    match choice.as_str() {
+        "y" => {
+            view_items();
+        },
+        "n" => {
+            menu_list();
+        },
+        _ => {
+            println!("invalid choice, sending you back to the main menu!");
+            menu_list();
+        }
+    }
 }
-
 fn view_items() {
-
     let info = "this is your ongoing items at the moment".blue();
     println!("{}", info);
-    
-    let content: String = fs::read_to_string("todo.txt")
-    .expect("File is missing from the directory!");
+
+    let content: String =
+        fs::read_to_string("todo.txt").expect("File is missing from the directory!");
 
     println!("{content}");
-
 }
 
 fn menu_list() {
+    println!("1. View ongoing stuff");
+    println!("2. Add ongoing stuff");
+    println!("3. Delete ongoing stuff");
+    println!("4. Check completed logs!");
+    println!("5. End the program");
 
-        println!("1. View ongoing stuff");
-        println!("2. Add ongoing stuff");
-        println!("3. Delete ongoing stuff");
-        println!("4. Check completed logs!");
-        println!("5. End the program");
-    
-        let mut choice = String::new();
-    
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("There was an issue when trying to get the choice info from user!");
-    
-        let choice: char = choice
-            .trim()
-            .parse()
-            .expect("missing value from previous function!");
-    
-        match choice {
-            '1' => view_items(),
-            '2' => add_items(),
-            '3' => delete_items(),
-            '4' => completed_logs(),
-            '5' => println!("thanks for using the system!"),
-            _ => println!("you have entered an invalid number, please enter the correct number"),
-        }    
-    
+    let mut choice = String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("There was an issue when trying to get the choice info from user!");
+
+    let choice: char = choice
+        .trim()
+        .parse()
+        .expect("missing value from previous function!");
+
+    match choice {
+        '1' => view_items(),
+        '2' => add_items(),
+        '3' => delete_items(),
+        '4' => completed_logs(),
+        '5' => println!("thanks for using the system!"),
+        _ => println!("you have entered an invalid number, please enter the correct number"),
+    }
 }
 
 fn main() {
@@ -62,29 +97,7 @@ fn main() {
     menu_list();
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-//   use std::process::Command;
-//   use std::str;
-
-//   #[test]
-//   fn test_menu_list() {
-//       // Run the program with '1' as the input
-//       let output = Command::new("cargo")
-//           .arg("run")
-//           .arg("--")
-//           .arg("1")
-//           .output()
-//           .expect("Failed to execute command");
-
-//       // Check the output
-//       let output_str = str::from_utf8(&output.stdout).unwrap();
-//       assert!(output_str.contains("1. View ongoing stuff"));
-//       assert!(output_str.contains("2. Add ongoing stuff"));
-//       assert!(output_str.contains("3. Delete ongoing stuff"));
-//       assert!(output_str.contains("4. Check completed logs!"));
-//       assert!(output_str.contains("5. End the program"));
-//       assert!(output_str.contains("this is a incomplete function"));
-//   }
-// }
+// TODO list:
+// 1 - finish all functions
+// 2- test all functions
+// 3 - integrate DB support (MongoDB)
