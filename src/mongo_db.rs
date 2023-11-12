e94dcf243cf69ee8bea56d51facb56eefb1b913d
Elgius <114131653+Elgius@users.noreply.github.com>
2023-11-12 09:13:23 +0500

    mongo files

--- src/mongo_db.rs
+++ src/mongo_db.rs
@@ -1,8 +1,23 @@
 use dotenv::{dotenv, var};
+use mongodb::bson::{doc, Document};
 use mongodb::options::ClientOptions;
 use mongodb::Client;
 use mongodb::Database;
 use tokio::runtime::Runtime;
+
+async fn export(db: Database) -> Result<(), mongodb::error::Error> {
+    let collection = db.collection::<Document>("books");
+
+    let doc = vec![
+        doc! { "title": "1984", "author": "George Orwell" },
+        doc! { "title": "Animal Farm", "author": "George Orwell" },
+        doc! { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
+    ];
+
+    collection.insert_many(doc, None).await?;
+
+    Ok(())
+}
 
 async fn client() -> Result<Database, mongodb::error::Error> {
     println!("client being loaded in");
@@ -18,10 +33,21 @@ async fn client() -> Result<Database, mongodb::error::Error> {
 pub fn mongo_time() {
     println!("initializing the mongo instance");
     let rt = Runtime::new().unwrap();
-    let db = rt.block_on(client());
+    let db_result = rt.block_on(client());
 
-    match db {
-        Ok(db) => println!("Connected to {}", db.name()),
-        Err(e) => eprintln!("Failed to connect to the database: {}",e),
+    match db_result {
+        Ok(db) => {
+            println!("Connected to {}", db.name());
+
+            println!("testing the export function");
+
+            let results = rt.block_on(export(db));
+
+            match results {
+                Ok(_) => println!("export has occured, very nice"),
+                Err(e) => eprintln!("failed to export the data: {}", e),
+            }
+        }
+        Err(e) => eprintln!("Failed to connect to the database: {}", e),
     }
 }
